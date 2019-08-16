2f7496f95578076d7ea08cf05d90f0cae60b4ec3
lionem2018 <lionem2018@gmail.com>
2019-08-16 18:51:39 +0900

    try new method for extracting basis characters

--- SixCaseClassifier.py
+++ SixCaseClassifier.py
@@ -158,14 +158,14 @@ def classify(labels, output_dir):
                 # pre_char1 = char_elements[1]
 
         ###
-        # if char_elements[2] == JONGSUNG_LIST[10]:
-        #     print(character)
-        #
-        # if char_elements[1] == JUNGSUNG_LIST[9] and char_elements[2] != JONGSUNG_LIST[0]:
+        # if char_elements[2] == JONGSUNG_LIST[20]:
         #     print(character)
 
-        if char_elements[1] == 'ㅞ' and char_elements[2] == 'ㅂ':
+        if char_elements[1] == JUNGSUNG_LIST[13] and char_elements[2] != JONGSUNG_LIST[0]:
             print(character)
+
+        # if char_elements[1] == 'ㅞ' and char_elements[2] == 'ㅂ':
+        #     print(character)
 
         ###
 
